e7f1d99905d60dfc1420b0af071b2d4d8bfb52db
1yan4min24 <162960381+1yan4min24@users.noreply.github.com>
2024-03-14 16:40:59 +0900

    Create Tugas3.py

---
+++ Tugas3.py
@@ -0,0 +1,13 @@
+dec = int(input("Masukkan bilangan desimal: "))
+
+# konversi ke biner
+biner = bin(dec)
+print("Hasil konversi ke biner:", biner)
+
+# konversi ke oktal
+oktal = oct(dec)
+print("Hasil konversi ke oktal:", oktal)
+
+# konversi ke heksadesimal
+heksa = hex(dec)
+print("Hasil konversi ke heksadesimal:", heksa)
